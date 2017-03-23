82ad9d9618c70352475a4df24ba08318fe54ccfc
Ben Blank <bblank@linkedin.com>
2017-03-23 12:05:46 -0700

    Implement command to convert instrumentation files to a JSONP file.

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  buildTree: require('./lib/build-tree'),
+  createNodeData: require('./lib/create-node-data').createNodeData,
+  getNodesById: require('./lib/get-nodes-by-id'),
+};
