f5d717fdbc8a8ac49984edce70f48f3564e9d9d8
Ben Blank <bblank@linkedin.com>
2017-03-27 14:11:29 -0700

    Add unzoom button, unzoom when switching trees.

--- src/broccoli-viz-sunburst.js
+++ src/broccoli-viz-sunburst.js
@@ -13,6 +13,8 @@ function loadData(trees) {
   const partition = d3.partition();
   const tooltip = d3.select('#tooltip');
   const treeSelector = d3.select('#trees');
+  const unzoomButton = d3.select('#unzoom');
+  let root;
   let svg;
 
   for (const tree in trees) {
@@ -28,6 +30,7 @@ function loadData(trees) {
   renderTree(trees[treeSelector.property('value')]);
 
   treeSelector.on('change', selectTree);
+  unzoomButton.on('click', unzoom);
 
   function anchorTooltip(/* d */) {
     tooltip.style('top', (d3.event.pageY - 10) + 'px');
@@ -35,6 +38,8 @@ function loadData(trees) {
   }
 
   function click(d) {
+    unzoomButton.attr('disabled', d === root ? 'disabled' : null);
+
     svg.transition()
       .duration(750)
       .tween('scale', function() {
@@ -69,9 +74,9 @@ function loadData(trees) {
   }
 
   function renderTree(tree) {
-    const root = d3.hierarchy(tree);
-
+    root = d3.hierarchy(tree);
     root.sum(function(d) { return d.selfTime; });
+    root = partition(root);
 
     d3.selectAll('svg').remove();
 
@@ -82,7 +87,7 @@ function loadData(trees) {
       .attr('transform', 'translate(' + width / 2 + ',' + (height / 2) + ')');
 
     svg.selectAll('path')
-      .data(partition(root).descendants())
+      .data(root.descendants())
       .enter().append('path')
       .attr('d', arc)
       .style('fill', function(d) { return color(d.data.label); })
@@ -94,9 +99,14 @@ function loadData(trees) {
 
   function selectTree() {
     renderTree(trees[d3.event.target.value]);
+    unzoom();
   }
 
   function timeInMs(ns) {
     return Math.round(ns / 1e6);
+  }
+
+  function unzoom() {
+    click(root);
   }
 }
